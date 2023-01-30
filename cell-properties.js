45708e29b2f24e53f105044f3e0b82aa31a27d6a
Sunny <sunnydubey>
2023-01-30 17:42:25 +0530

    Formula Evaluation

--- cell-properties.js
+++ cell-properties.js
@@ -13,7 +13,9 @@ for (let i = 0; i < rows; i++) {
             fontFamily: "monospace",
             fontSize: "14",
             fontColor: "#000000",
-            BGcolor: "#000000",  // Just for indication purpose,
+            BGcolor: "#000000",  // Just for indication purpose
+            value: "",
+            formula: "",
         }
         sheetRow.push(cellProp);
     }
@@ -42,7 +44,7 @@ let inactiveColorProp = "#ecf0f1";
 // Attach property listeners
 bold.addEventListener("click", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     // Modification
     cellProp.bold = !cellProp.bold; // Data change
@@ -52,7 +54,7 @@ bold.addEventListener("click", (e) => {
 
 italic.addEventListener("click", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     // Modification
     cellProp.italic = !cellProp.italic; // Data change
@@ -62,7 +64,7 @@ italic.addEventListener("click", (e) => {
 
 underline.addEventListener("click", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     // Modification
     cellProp.underline = !cellProp.underline; // Data change
@@ -72,7 +74,7 @@ underline.addEventListener("click", (e) => {
 
 fontSize.addEventListener("change", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     cellProp.fontSize = fontSize.value; // Data change
     cell.style.fontSize = cellProp.fontSize + "px";
@@ -81,7 +83,7 @@ fontSize.addEventListener("change", (e) => {
 
 fontFamily.addEventListener("change", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     cellProp.fontFamily = fontFamily.value; // Data change
     cell.style.fontFamily = cellProp.fontFamily;
@@ -90,7 +92,7 @@ fontFamily.addEventListener("change", (e) => {
 
 fontColor.addEventListener("change", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     cellProp.fontColor = fontColor.value; // Data change
     cell.style.color = cellProp.fontColor;
@@ -99,7 +101,7 @@ fontColor.addEventListener("change", (e) => {
 
 BGcolor.addEventListener("change", (e) => {
     let address = addressBar.value;
-    let [cell, cellProp] = activecell(address);
+    let [cell, cellProp] = getCellAndCellProp(address);
 
     cellProp.BGcolor = BGcolor.value; // Data change
     cell.style.backgroundColor = cellProp.BGcolor;
@@ -109,7 +111,7 @@ BGcolor.addEventListener("change", (e) => {
 alignment.forEach((alignElem) => {
     alignElem.addEventListener("click", (e) => {
         let address = addressBar.value;
-        let [cell, cellProp] = activecell(address);
+        let [cell, cellProp] = getCellAndCellProp(address);
 
         let alignValue = e.target.classList[0];
         cellProp.alignment = alignValue; // Data change
@@ -191,7 +193,7 @@ function addListenerToAttachCellProperties(cell) {
     })
 }
 
-function activecell(address) {
+function getCellAndCellProp(address) {
     let [rid, cid] = decodeRIDCIDFromAddress(address);
     // Access cell & storage object
     let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
---
+++ formula.js
@@ -0,0 +1,50 @@
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
+        cell.addEventListener("blur", (e) => {
+            let address = addressBar.value;
+            let [activeCell, cellProp] = getCellAndCellProp(address);
+            let enteredData = getCellAndCellProp.innerText;
+
+            cellProp.value = enteredData;
+
+        })
+    }
+}
+
+let formulaBar = document.querySelector(".formula-bar");
+formulaBar.addEventListener("keydown", (e) => {
+    let inputFormula = formulaBar.value;
+    if (e.key == "Enter" && inputFormula) {
+        let evaluatedValue = evaluateFormula(inputFormula);
+
+        // To update UI and cellProp in DB
+        SetCellUIAndCellProp(evaluatedValue, inputFormula);
+    }
+})
+
+function evaluateFormula(formula) {
+    let encodedFormula = formula.split(" ");
+    for (let i = 0; i < encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if (asciiValue >= 65 && asciiValue <= 90) {
+            let [cell, cellProp] = getCellAndCellProp(encodedFormula[i]);
+            encodedFormula[i] = cellProp.value;
+        }
+    }
+    let decodedFormula = encodedFormula.join(" ");
+    return eval(decodedFormula);
+}
+
+function SetCellUIAndCellProp(evaluatedValue, formula) {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    // UI update
+    cell.innerText = evaluatedValue;
+    // DB update
+    cellProp.value = evaluatedValue;
+    cellProp.formula = formula;
+
+
+}
